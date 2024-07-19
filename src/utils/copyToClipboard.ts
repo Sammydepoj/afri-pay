8cec5e0470cda257e36e112013f7c2bd60898ead
Samuel Adepoju <samueladepoju86@yahoo.com>
2024-07-19 09:34:39 +0100

    feat:fixed issues with copy to clipboard and download transaction reciept

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,7 +1,34 @@
 export async function copyTextToClipboard(text: string) {
-  if ("clipboard" in navigator) {
-    return await navigator.clipboard.writeText(text);
-  } else {
-    return document.execCommand("copy", true, text);
+  if (!navigator.clipboard) {
+    console.error("Clipboard API is not available.");
+    return;
+  }
+
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log("Text copied to clipboard successfully!");
+  } catch (err) {
+    console.error("Failed to copy text to clipboard using Clipboard API:", err);
+    try {
+      const textarea = document.createElement("textarea");
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      const successful = document.execCommand("copy");
+      document.body.removeChild(textarea);
+
+      if (successful) {
+        console.log(
+          "Text copied to clipboard using execCommand as a fallback!"
+        );
+      } else {
+        console.error("Failed to copy text to clipboard using execCommand.");
+      }
+    } catch (fallbackErr) {
+      console.error(
+        "Failed to copy text to clipboard using fallback method:",
+        fallbackErr
+      );
+    }
   }
 }
--- src/views/dashboard/DownloadModal.tsx
+++ src/views/dashboard/DownloadModal.tsx
@@ -2,6 +2,11 @@ import { useAppDispatch, useAppSelector } from "../../store/hooks";
 import { PageModal } from "../../common/components/modal";
 import { setAllGlobalKey } from "../../store";
 import { TableDataTypes } from "../../common/components/TransactionsTable";
+import DownloadIcon from "../../assets/icons/DownloadIcon";
+import { Button } from "antd";
+import html2canvas from "html2canvas";
+import fileDownload from "js-file-download";
+import React, { useRef } from "react";
 
 const DownloadModal = ({
   selectedTransaction,
@@ -12,7 +17,16 @@ const DownloadModal = ({
   const state = useAppSelector((state) => {
     return state.global;
   });
+  const contentRef = useRef<HTMLDivElement>(null);
 
+  const handleDownload = async () => {
+    if (contentRef.current) {
+      const canvas = await html2canvas(contentRef.current);
+      const dataUrl = canvas.toDataURL("image/png");
+      const blob = await fetch(dataUrl).then((res) => res.blob());
+      fileDownload(blob, "transaction-details.png");
+    }
+  };
   return (
     <PageModal
       openModal={state.showDownloadModal}
@@ -24,70 +38,75 @@ const DownloadModal = ({
       closable
       modalWith="25rem"
     >
-      <h5 className="text-lg font-[ibm-bold]">Transaction Details</h5>
-      <div className="bg-[#F9FBF9] my-4 p-4 flex flex-col gap-4 rounded-lg">
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">Amount</p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            ₦{selectedTransaction?.amount}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">
-            Terminal ID
-          </p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.terminalId}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">
-            Transaction date
-          </p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.transactionDate}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">Time</p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.timeLocalTransaction}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">STAN</p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.stan}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">Masked PAN</p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.pan}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">Status</p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.transactionStatus}
-          </p>
-        </span>
-        <span className="flex items-center justify-between">
-          <p className="text-[#284E32] text-sm font-[ibm-medium]">
-            Response Code
-          </p>
-          <p className="text-[#284E32] text-base font-[ibm-semibold]">
-            {selectedTransaction?.responseCode}
-          </p>
-        </span>
+      <div ref={contentRef}>
+        <h5 className="text-lg font-[ibm-bold]">Transaction Details</h5>
+        <div className="bg-[#F9FBF9] my-4 p-4 flex flex-col gap-4 rounded-lg">
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">Amount</p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              ₦{selectedTransaction?.amount}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">
+              Terminal ID
+            </p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.terminalId}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">
+              Transaction date
+            </p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.transactionDate}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">Time</p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.timeLocalTransaction}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">STAN</p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.stan}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">
+              Masked PAN
+            </p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.pan}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">Status</p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.transactionStatus}
+            </p>
+          </span>
+          <span className="flex items-center justify-between">
+            <p className="text-[#284E32] text-sm font-[ibm-medium]">
+              Response Code
+            </p>
+            <p className="text-[#284E32] text-base font-[ibm-semibold]">
+              {selectedTransaction?.responseCode}
+            </p>
+          </span>
+        </div>
       </div>
       <div className="flex items-center justify-center">
-        {/* <Button
+        <Button
           icon={<DownloadIcon />}
           className="py-6 bg-[#374D41] hover:!bg-[#374D41] text-white hover:!text-white"
+          onClick={handleDownload}
         >
           Download
-        </Button> */}
+        </Button>
       </div>
     </PageModal>
   );
